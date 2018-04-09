981afb966814bce9e15d98a88e4356401342c26b
subik.pokharel <subikpokharel93@gmail.com>
2018-04-09 14:21:50 -0500

    initial commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Search | Engine</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="public/bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="public/template.min.css">
+
+  <!-- Reference to Jquery script -->
+    <script scr ="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+
+</head>
+<body class="hold-transition skin-blue ">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3"style="font-size:25px; margin-top:10px;">    			
+				<a href="." class="logo">
+      					<span class="logo-lg"><b>Search </b> Engine</span>
+    				</a>
+			</div>
+  		</div>
+		
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+	require_once('header.php');
+
+
+	require_once('class/GetData.class.php');
+	//require_once( 'class/Test.class.php');
+
+
+	$data = new GetData();
+
+	//$data = new Test();
+	
+	if (isset($_POST['btnSubmit'])) {
+	//	print_r($_POST);
+		$err = array();
+
+		if (isset($_POST['searchBar']) && !empty($_POST['searchBar'])) {
+			$data->url =  $_POST['searchBar'];
+			$data->action = $_POST['action'];
+		} else {
+			$err['searchBar'] = "Please enter a URL";
+		}
+
+
+		if (count($err) == 0) {
+			$received = $data->getData();
+		}
+	}
+
+?>
+
+
+<div class = "row" style=" margin-top:50px;">
+	<div class = "col-md-2"></div>
+	<?php if (isset($err['searchBar'])) {?>
+		<div class="alert alert-danger alert-dismissable col-md-8">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+			<?php echo $err['searchBar'];?>
+		</div>
+	<?php }?>
+
+
+	<?php if (isset($received)) {?>
+		<div class="alert alert-danger alert-dismissable col-md-8">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+			<?php echo $received;?>
+		</div>
+	<?php }?>
+
+</div>
+
+
+
+
+<div class = "row">
+	<div class = "col-md-2"></div>
+	<div class="box-tools ">
+		<form method="post" id="Search_Bar">   <!-- action="../../cgi-bin/GetData.pl" -->
+			<input type="hidden" class="form-control" name = "action" value="getData">
+			<div class="input-group input-group-lg col-md-8 ">
+                		<input type="text" name="searchBar" class="form-control" placeholder="Enter the url for data to be extracted..."/>
+				<div class="input-group-btn">
+					<button type="submit" class="btn btn-default" name="btnSubmit"><i>Submit</i></button>
+                  		</div>
+                	</div>
+		</form>
+	</div>
+	<div class = "col-md-2"></div>
+</div>
+
+
+
+
+<?php
+
+	require_once('footer.php');
+?>
